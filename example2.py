cc77bd0755fed801fd25d7dd536d7f5ac313761d
Aniket Kumar <aniketsinha101@gmail.com>
2021-06-27 18:10:16 +0530

    Create example2.py

---
+++ example2.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI 
+from pydantic import BaseModel
+app=FastAPI()
+
+class Item(BaseModel):
+  name:str
+  price:float
+@app.get('/')
+def  read():
+  return {"hello","world"}
+  
+//Dynamic query
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}
+e
